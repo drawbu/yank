b6221a661686224c96d50c63c23959daf530f7e2
Clément <git@drawbu.dev>
2026-09-01 10:48:28 +0200

    build: set up the crate

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! `yank` shares one clipboard across the machines you own.
+
+#![warn(clippy::pedantic)]
+#![allow(clippy::doc_markdown)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_panics_doc)]
+
+
